c9671cd1badf4603d01df976383868f0cafbc26f
Cblpka <vehluncho@gmail.com>
2017-11-28 17:20:11 +0300

    Initial Commit

---
+++ Giphy Viewer (TT for Perfectorium)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Giphy Viewer (TT for Perfectorium)
+//
+//  Created by Admin on 28.11.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Giphy Viewer (TT for Perfectorium)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Giphy Viewer (TT for Perfectorium)
+//
+//  Created by Admin on 28.11.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
